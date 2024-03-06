fd5d6049cbdab5cb1df821abcb5398784a25ab7b
Qianyi Liu <ql309@cam.ac.uk>
2024-03-06 21:30:17 +0000

    Fix multiplier to be self.lin.weight in scaled_fixed time encoder

--- time_encoders.py
+++ time_encoders.py
@@ -120,7 +120,7 @@ class FixedCosTimeEncoder(nn.Module, TimeEncoder):
     """Cosine time encoder with non-learnable exponential range of frequencies"""
 
     def __init__(
-        self, out_channels: int, mul: float = 1.0, parameter_requires_grad: bool = True
+        self, out_channels: int, mul: float = 1.0, parameter_requires_grad: bool = False
     ):
         """
         Time encoder.
@@ -199,8 +199,7 @@ class ScaledFixedCosTimeEncoder(nn.Module, TimeEncoder):
         # Tensor, shape (batch_size, seq_len, 1)
         timestamps = timestamps.unsqueeze(-1)
 
-        multiplier = torch.exp(-self.lin.weight**2)
-        # multiplier = self.lin.weight
+        multiplier = self.lin.weight
         output = torch.matmul(timestamps, multiplier.t())
         if output.shape[0] != 0:
             # TODO handle var shape
